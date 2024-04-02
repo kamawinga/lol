c50cc4a94157b11241fd5fb2fc73d49390cd856b
alfellati <jbashir52@gmail.com>
2024-04-02 23:20:33 +0800

    rem out

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV = 'production';
+
 const nextConfig = {
+  basePath: isProd ? '/website' : '',
   output: 'export',
+  distDir: 'dist',
   images: {
     unoptimized: true,
   },
