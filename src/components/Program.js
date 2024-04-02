227dc07ce2d81a23ef5e264564081f36e6630df1
Muhammad-Jibril B.A. (Khalifa MBA) <jbashir52@gmail.com>
2024-04-02 18:33:36 +0800

    three grids

--- src/components/Program.js
+++ src/components/Program.js
@@ -1,14 +1,12 @@
-import React, { useEffect, useRef } from 'react';
-import profilePic from "../../public/images/profile/kamaprofile.png";
-import Image from 'next/image';
+import React from 'react';
 
 const Program = () => {
   return (
     <div className='my-64'>
-        <div className='grid w-full grid-cols-8 gap-16 sm:gap-8'>
+        <div className='w-full grid grid-cols-3 gap-16 sm:gap-8 items-center justify-center'>
             
-            <div className='col-span-4 relative h-max rounded-2xl border-2 border-solid border-dark
-            bg-light p-8 dark:bg-dark dark:border-light xl:col-span-4 md:order-1 md:col-span-8'
+            <div className='col-span-1 relative h-max rounded-2xl border-2 border-solid border-dark
+            bg-light p-8 dark:bg-dark dark:border-light xl:col-span-1 md:order-1 md:col-span-8'
             >
                 <div
                     className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light'
@@ -26,8 +24,8 @@ const Program = () => {
                 </div>
             </div>
 
-            <div className='col-span-4 relative h-max rounded-2xl border-2 border-solid border-dark
-            bg-light p-8 dark:bg-dark dark:border-light xl:col-span-4 md:order-1 md:col-span-8'
+            <div className='col-span-1 relative h-max rounded-2xl border-2 border-solid border-dark
+            bg-light p-8 dark:bg-dark dark:border-light xl:col-span-1 md:order-1 md:col-span-8'
             >
                 <div
                     className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light'
@@ -45,27 +43,8 @@ const Program = () => {
                 </div>
             </div>
 
-            <div className='col-span-4 relative h-max rounded-2xl border-2 border-solid border-dark
-            bg-light p-8 dark:bg-dark dark:border-light xl:col-span-4 md:order-1 md:col-span-8'
-            >
-                <div
-                    className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light'
-                />
-                <div className='flex w-full flex-col items-center justify-center font-bold text-lg pt-6 text-primary dark:text-primaryDark'>
-                    <h1>$KAMA Memecoin</h1>
-                </div>
-                <div className='flex w-full flex-col items-center justify-center font-bold text-sm'>
-                    <h1>$KAMA Memecoin</h1>
-                    <h1>$KAMA Memecoin</h1>
-                </div>
-                <div className='flex w-full flex-col items-center justify-center font-bold text-xs text-dark/75 dark:text-light/75'>
-                    <p>SDE| TPM| Architect| Researcher</p>
-                    <p>(STAR)</p>
-                </div>
-            </div>
-
-            <div className='col-span-4 relative h-max rounded-2xl border-2 border-solid border-dark
-            bg-light p-8 dark:bg-dark dark:border-light xl:col-span-4 md:order-1 md:col-span-8'
+            <div className='col-span-`1` relative h-max rounded-2xl border-2 border-solid border-dark
+            bg-light p-8 dark:bg-dark dark:border-light xl:col-span-`1` md:order-1 md:col-span-8'
             >
                 <div
                     className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-light'
