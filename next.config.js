5065ea91b12c6798659f2a42288054dd68a90aa3
alfellati <jbashir52@gmail.com>
2024-04-02 21:33:10 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export',
+  distDir: 'dist',
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
