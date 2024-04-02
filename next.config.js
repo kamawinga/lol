902f7f5d2c28cc852e0e48f0dc5275f6c8531f89
alfellati <jbashir52@gmail.com>
2024-04-02 23:21:30 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 const isProd = process.env.NODE_ENV = 'production';
 
 const nextConfig = {
-  basePath: isProd ? '/website' : '',
+  basePath: isProd ? '/lol' : '',
   output: 'export',
   distDir: 'dist',
   images: {
