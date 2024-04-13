b59c56be15adc9a530744be253d346cce211f52d
alfellati <jbashir52@gmail.com>
2024-04-14 02:59:37 +0800

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -120,7 +120,7 @@ const NavBar = () => {
 
                     <button
                     onClick={() => setMode(mode === 'light'? 'dark' : 'light')}
-                    className={`ml-3 flex items-center justify-center rounded-full p-1
+                    className={`w-6 ml-3 flex items-center justify-center rounded-full p-1
                     ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"}
                     `}
                     >
@@ -132,7 +132,6 @@ const NavBar = () => {
                     </button>
                 </nav>
             </div>
-            
         </div>
                         
         {
